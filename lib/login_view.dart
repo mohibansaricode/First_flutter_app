ec24557c5fa1f496f1af680d542d3f19654acb5c
mohibansaricode <mmcode46@gmail.com>
2025-05-11 15:16:50 +0500

    first Counter Application

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  int counterCount = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurpleAccent,
-        title: Text("data"),
+        backgroundColor: Colors.black,
+        title: Text(
+          "COUNTER APPLICTAION",
+          style: TextStyle(fontSize: 25, color: Colors.white),
+        ),
       ),
-      body: Container(
-        color: Colors.yellow,
-        height: 100,
-        width: 100,
-        child: Text("Hello World"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$counterCount", style: TextStyle(fontSize: 40)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counterCount++;
+                    });
+                  },
+                  child: Icon(Icons.add, color: Colors.blueAccent, size: 24.0),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (counterCount > 0) {
+                        counterCount--;
+                      }
+                    });
+                  },
+                  child: Icon(
+                    Icons.minimize_rounded,
+                    color: Colors.red,
+                    size: 24.0,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counterCount = 0;
+                    });
+                  },
+                  child: Icon(
+                    Icons.restart_alt,
+                    color: Colors.deepPurple,
+                    size: 24.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
